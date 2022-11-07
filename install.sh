c597e33c9992b15fefa3b50e9dc438d4576fa1f0
sayserx <106224942+sayserx@users.noreply.github.com>
2022-11-07 14:12:29 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+clear
+npm install fb-chat-api
+npm install facebook-chat-api
+npm install axios
+npm install request
+npm install system-sleep
+npm install system-sleep
+npm install string.prototype.startswith
+echo "node bot.js <idyour";
